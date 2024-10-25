19b3a1a0b3eca6cf84159779a2cb55f97d163353
dhiraj gurung <gdhiraj030@gmail.com>
2024-10-25 19:13:40 +0300

    mobile navigation and FAQ add

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,105 @@
-// Disable right-click context menu
-document.addEventListener("contextmenu", function (e) {
-  if (e.target.tagName === "IMG") {
-    e.preventDefault();
-  }
+"use strict";
+
+const hamburgerOpen = document.querySelector(".hamburger__open");
+const hamburgerClose = document.querySelector(".hamburger__close");
+const mobileNav = document.querySelector(".page1__mobile__nav");
+const faqContainer = document.querySelector(".page6__faq__container");
+const plusButton = document.querySelector(".plus ");
+
+const faqQuestion = document.querySelectorAll(".page6__faq--question");
+
+faqQuestion.forEach((qsn) => {
+  console.log("hello");
+  qsn.addEventListener("click", () => {
+    const answer = qsn.nextElementSibling;
+    const height = answer.scrollHeight;
+    if (answer.style.height === "0px") {
+      answer.style.height = height + "px";
+      plusButton.style.transform = "rotate(45deg)";
+    } else {
+      answer.style.height = "0px";
+      plusButton.style.transform = "rotate(0deg)";
+    }
+  });
 });
 
-// Disable drag and drop
-document.addEventListener("dragstart", function (e) {
-  if (e.target.tagName === "IMG") {
-    e.preventDefault();
-  }
-});
+function smoothScroll() {
+  gsap.registerPlugin(ScrollTrigger);
 
-// Disable image selection
-document.addEventListener("selectstart", function (e) {
-  if (e.target.tagName === "IMG") {
-    e.preventDefault();
-  }
-});
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+smoothScroll();
+
+// NAVIGATION TOGGLE
+function navToggle() {
+  hamburgerOpen.addEventListener("click", function (e) {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".hamburger__open",
+      { rotate: 0 },
+      {
+        rotate: 360,
+        duration: 0.6,
+        ease: "power3.inout",
+      }
+    );
+    tl.to(".page1__mobile__nav", {
+      x: 0,
+      duration: 0.6,
+      ease: "power3.inout",
+    });
+  });
+
+  hamburgerClose.addEventListener("click", function (e) {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".hamburger__close",
+      { rotate: 0 },
+      {
+        rotate: 360,
+        duration: 0.6,
+        ease: "power3.inout",
+      }
+    );
+    tl.to(".page1__mobile__nav", {
+      x: "100%",
+      duration: 0.6,
+      ease: "power3.inout",
+    });
+  });
+}
+navToggle();
